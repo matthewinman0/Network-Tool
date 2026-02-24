29854412e671f4c9246bb3d9402e5033764725cb
Matthew Inman <matthewinman321@gmail.com>
2026-02-24 08:02:28 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,3 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     kotlin("android") version "1.9.22" apply false
 }
-
-dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-
-    // Material 3
-    implementation("com.google.android.material:material:1.11.0")
-
-    // Lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-
-    // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
-}
