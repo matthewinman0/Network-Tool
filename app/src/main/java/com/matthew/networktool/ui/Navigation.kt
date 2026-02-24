77665932ae728e0f3b72fc79645ff6956ff68d92
Matthew Inman <matthewinman321@gmail.com>
2026-02-24 12:40:10 +0000

    Update Navigation.kt

--- app/src/main/java/com/matthew/networktool/ui/Navigation.kt
+++ app/src/main/java/com/matthew/networktool/ui/Navigation.kt
@@ -1,4 +1,4 @@
-
+package com.matthew.networktool.ui
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.ui.Modifier
@@ -8,7 +8,6 @@ import androidx.compose.material3.*
 import androidx.compose.material.icons.*
 import androidx.compose.material.icons.filled.*
 import androidx.compose.ui.graphics.vector.ImageVector
-package com.matthew.networktool.ui.*
 import androidx.compose.material3.Surface
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
