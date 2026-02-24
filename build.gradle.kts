0cc84a733ce7b215ac687ce62454563f220e9fd6
Matthew Inman <matthewinman321@gmail.com>
2026-02-24 11:05:02 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     kotlin("android") version "1.9.22" apply false
+
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+}
 }
