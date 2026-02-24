9da9854a0f05d58e751029bd78d41c3e8e49b936
Matthew Inman <matthewinman321@gmail.com>
2026-02-24 12:27:26 +0000

    Update ToolsScreen.kt

--- app/src/main/java/com/matthew/networktool/ui/ToolsScreen.kt
+++ app/src/main/java/com/matthew/networktool/ui/ToolsScreen.kt
@@ -1,5 +1,11 @@
-import androidx.compose.runtime.*
+import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ToolsScreen() {
