422ed32380e09754748014412704dad61b7a85df
Matthew Inman <matthewinman321@gmail.com>
2026-02-24 00:16:11 +0000

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
-rootProject.name = "network-tool"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NetworkTool"
 include(":app")
