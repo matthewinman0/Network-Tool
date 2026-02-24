d3b8043597fb6a8505b307c0da12092155f630dc
Matthew Inman <matthewinman321@gmail.com>
2026-02-24 09:39:19 +0000

    Add SettingsScreen composable for settings UI

---
+++ app/src/main/java/com/matthew/networktool/ui/SettingsScreen.kt
@@ -0,0 +1,8 @@
+@Composable
+fun SettingsScreen() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text("Settings", style = MaterialTheme.typography.headlineMedium)
+    }
+}
