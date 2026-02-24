ebff512ac082d17dbab002ab4913e35ebb90e550
Matthew Inman <matthewinman321@gmail.com>
2026-02-24 09:36:12 +0000

    Add navigation-compose dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,5 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
